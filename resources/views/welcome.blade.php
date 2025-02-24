f6a550b2f6a0ebacd30fd23aabfe4506af617c1e
Soe Myat Cho <soemyatchobgn@gmail.com>
2025-02-23 23:52:03 -0800

    add header

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite ('resources/css/app.css')
+</head>
+<body class="bg-gray-100 text-gray-900 flex flex-col min-h-screen">
+    <header class="bg-blue-500 text-white text-center p-4 text-xl font-bold">
+        Simple Webpage
+    </header>
+    
+</body>
+</html>
